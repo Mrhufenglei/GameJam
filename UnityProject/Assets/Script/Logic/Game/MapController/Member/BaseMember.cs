812af9b892cb791825d3a8d23f20a1838eb06a0d
hufenglei <1576543259@qq.com>
2021-02-07 16:43:31 +0800

    1、添加移动

--- UnityProject/Assets/Script/Logic/Game/MapController/Member/BaseMember.cs
+++ UnityProject/Assets/Script/Logic/Game/MapController/Member/BaseMember.cs
@@ -13,13 +13,18 @@ public class BaseMember : MonoBehaviour
 {
     public MemberType m_memberType = MemberType.Enemy;
 
+    [Header("Collider")]
     public CharacterController m_collider;
 
+    [Header("Move Speed")]
     public float m_speedX = 10;
     public float m_speedY = 10;
 
     public void Move(float montionX, float montionY, float montionZ)
     {
-        if (m_collider != null) m_collider.Move(new Vector3(montionX * m_speedX, montionY, montionZ * m_speedY));
+        if (m_collider != null)
+        {
+            m_collider.Move(new Vector3(montionX * m_speedX, montionY, montionZ * m_speedY));
+        }
     }
 }
