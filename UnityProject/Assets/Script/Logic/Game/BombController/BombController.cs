e9f38b86fe2a512939ed8b3b0b67b236bada2f4b
hufenglei <1576543259@qq.com>
2021-02-08 14:51:02 +0800

    1、添加波数可控制

--- UnityProject/Assets/Script/Logic/Game/BombController/BombController.cs
+++ UnityProject/Assets/Script/Logic/Game/BombController/BombController.cs
@@ -4,24 +4,38 @@
 //
 //----------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 /// <summary>
 /// 
 /// </summary>
 public class BombController : MonoBehaviour, IGameController
 {
-    public GameObject BombPrefab;
+    [Serializable]
+    public class WavaData
+    {
+        public List<WavaBombData> m_wavaBombDatas = new List<WavaBombData>();
+    }
 
-    public float DropInterval = 5;
-    public float DropCount = 5;
+    [Serializable]
+    public class WavaBombData
+    {
+        public int m_index;
+        public int m_count;
+    }
 
+    [Header("Wava Setting")] public GameObject[] m_prefabs;
+    public List<WavaData> m_wavaData = new List<WavaData>(1);
+    public int m_wavaIndex = 0;
+
+    [Header("Bound Setting")] public float DropInterval = 5;
     public float DropY = 10;
-
     public float RandomRangeX = 5;
     public float RandomRangeZ = 5;
-    private float mTimer = 0f;
+    [Header("Create Setting")] private float mTimer = 0f;
 
     private List<BombBase> m_bombs = new List<BombBase>();
     private Dictionary<int, BombBase> m_dicBombs = new Dictionary<int, BombBase>();
@@ -30,6 +44,7 @@ public class BombController : MonoBehaviour, IGameController
 
     public void OnInit()
     {
+        m_wavaIndex = 0;
     }
 
     public void OnUpdate(float deltaTime, float unscaledDeltaTime)
@@ -37,9 +52,13 @@ public class BombController : MonoBehaviour, IGameController
         mTimer -= Time.deltaTime;
         if (mTimer <= 0)
         {
-            for (int i = 0; i < DropCount; i++)
+            CreateBomb();
+
+            m_wavaIndex++;
+
+            if (m_wavaIndex >= m_wavaData.Count)
             {
-                CreateBomb();
+                m_wavaIndex = m_wavaData.Count - 1;
             }
 
             mTimer = DropInterval;
@@ -71,15 +90,28 @@ public class BombController : MonoBehaviour, IGameController
 
     private void CreateBomb()
     {
-        var go = GameObject.Instantiate(BombPrefab);
-        var rndx = Random.Range(-RandomRangeX, RandomRangeX);
-        var rndz = Random.Range(-RandomRangeZ, RandomRangeZ);
-        var bombBase = go.GetComponent<BombBase>();
-        if (bombBase == null) return;
+        WavaData data = GetWavaData();
+        for (int i = 0; i < data.m_wavaBombDatas.Count; i++)
+        {
+            var wava = data.m_wavaBombDatas[i];
+            var prefab = m_prefabs[wava.m_index];
+            var count = wava.m_count;
+            if (prefab == null) continue;
+            if (count == 0) continue;
 
-        go.transform.position = new Vector3(rndx, DropY, rndz);
-        m_bombs.Add(bombBase);
-        m_dicBombs[go.GetInstanceID()] = bombBase;
+            for (int j = 0; j < count; j++)
+            {
+                var obj = GameObject.Instantiate(prefab);
+                var rndx = Random.Range(-RandomRangeX, RandomRangeX);
+                var rndz = Random.Range(-RandomRangeZ, RandomRangeZ);
+                var bombBase = obj.GetComponent<BombBase>();
+                if (bombBase == null) return;
+
+                obj.transform.position = transform.TransformPoint(new Vector3(rndx, DropY, rndz));
+                m_bombs.Add(bombBase);
+                m_dicBombs[obj.GetInstanceID()] = bombBase;
+            }
+        }
     }
 
     public void DestroyBomb(BombBase bombBase)
@@ -110,5 +142,20 @@ public class BombController : MonoBehaviour, IGameController
     public BombBase GetBomb(int key)
     {
         return m_dicBombs[key];
+    }
+
+    public WavaData GetWavaData()
+    {
+        if (m_wavaIndex >= m_wavaData.Count)
+        {
+            m_wavaIndex = m_wavaData.Count - 1;
+        }
+
+        return m_wavaData[m_wavaIndex];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(RandomRangeX * 2, DropY, RandomRangeZ * 2));
     }
 }
